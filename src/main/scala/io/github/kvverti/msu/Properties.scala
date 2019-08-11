b8334432077f143a75dd433f1f5714751272cf17
Thalia Nero <chrisvn00@gmail.com>
2019-08-10 22:48:24 -0400

    Documentation

--- src/main/scala/io/github/kvverti/msu/Properties.scala
+++ src/main/scala/io/github/kvverti/msu/Properties.scala
@@ -11,26 +11,36 @@ import java.util.Optional
 import net.minecraft.state.PropertyContainer
 import net.minecraft.state.property.Property
 
+/**
+ * Implicit utilities for working with properties and property containers.
+ */
 object Properties {
 
+
+  implicit class ObjPropertyWrapper[A <: Comparable[A]](val self: Property[A]) extends AnyVal {
+    def getVal(name: String): Option[A] = self.getValue(name).toOption
+  }
+
   implicit class IntPropertyWrapper(val self: Property[Integer]) extends AnyVal {
-    def getIntValue(name: String): Option[Int] = self.getValue(name).toOption.map(Int.unbox)
+    def getVal(name: String): Option[Int] = self.getValue(name).toOption.map(Int.unbox)
   }
 
   implicit class BooleanPropertyWrapper(val self: Property[java.lang.Boolean]) extends AnyVal {
-    def getBoolValue(name: String): Option[Boolean] = self.getValue(name).toOption.map(Boolean.unbox)
+    def getVal(name: String): Option[Boolean] = self.getValue(name).toOption.map(Boolean.unbox)
   }
 
+  // this isn't actually the real type of the map either, but it's close enough
+  // to be useful
+  import scala.language.existentials
+  type Entries = ImmutableMap[Property[A], A] forSome { type A <: Comparable[A] }
+
   implicit class PropertyContainerFixer[C <: PropertyContainer[C]](val self: C) extends AnyVal {
-    // this isn't actually the real type of the map either, but it's close enough
-    // to be useful
-    import scala.language.existentials
-    type Entries = ImmutableMap[Property[A], A] forSome { type A <: Comparable[A] }
     def entries: Entries = self.getEntries.asInstanceOf[Entries]
-    def getInt(prop: Property[Integer]): Int = Int.unbox(self.get(prop))
-    def getBool(prop: Property[java.lang.Boolean]): Boolean = Boolean.unbox(self.get(prop))
-    def withVal[A <: Comparable[A]](prop: Property[A], value: A): C = self.`with`(prop, value)
+    def getVal(prop: Property[Integer]): Int = Int.unbox(self.get(prop))
+    def getVal(prop: Property[java.lang.Boolean]): Boolean = Boolean.unbox(self.get(prop))
+    def getVal[A <: Comparable[A]](prop: Property[A]): A = self.get(prop)
     def withVal(prop: Property[Integer], value: Int): C = self.`with`(prop, Int.box(value))
     def withVal(prop: Property[java.lang.Boolean], value: Boolean): C = self.`with`(prop, Boolean.box(value))
+    def withVal[A <: Comparable[A]](prop: Property[A], value: A): C = self.`with`(prop, value)
   }
 }
--- src/main/scala/io/github/kvverti/msu/WorldAccess.scala
+++ src/main/scala/io/github/kvverti/msu/WorldAccess.scala
@@ -7,10 +7,25 @@ import net.minecraft.world.BlockView
 
 import scala.reflect.ClassTag
 
+/**
+ * Implicit extensions for accessing Blocks and BlockEntities
+ * in a type safe manner.
+ */
 object WorldAccess {
 
   implicit class RichBlockState(val self: BlockState) extends AnyVal {
 
+    /**
+     * Get a block of a certain type, or None if the block is not of that type.
+     * Useful in for comprehensions like so.
+     * {{{
+     *  for {
+     *    pos <- neighbors
+     *    state = world.getBlockState(pos)
+     *    block <- state.typedBlock[MyBlock]
+     *  } yield block.myMethod(state, world, pos)
+     * }}}
+     */
     def typedBlock[B <: Block: ClassTag]: Option[B] = {
       self.getBlock match {
         case b: B => Some(b)
@@ -18,12 +33,32 @@ object WorldAccess {
       }
     }
 
+    /**
+     * Apply the given partial function to this state's block, and return
+     * the result. Useful for applying a single operation to a blockstate.
+     * {{{
+     *  state.collectBlock {
+     *    case b: MyBlock => b.myMethod(state, world, pos)
+     *  }
+     * }}}
+     */
     def collectBlock[A](pf: PartialFunction[Block, A]): Option[A] =
       Option(self.getBlock) collect pf
   }
 
   implicit class RichWorld(val self: BlockView) extends AnyVal {
 
+    /**
+     * Get a block entity of a certain type, or None if the block is not of
+     * that type. Useful in for comprehensions like so.
+     * {{{
+     *  for {
+     *    pos <- neighbors
+     *    be <- world.typedBlockEntity[MyBlockEntity](pos)
+     *    state = world.getBlockState(pos)
+     *  } yield be.myMethod(state, world, pos)
+     * }}}
+     */
     def typedBlockEntity[B <: BlockEntity: ClassTag](pos: BlockPos): Option[B] = {
       self.getBlockEntity(pos) match {
         case b: B => Some(b)
@@ -31,11 +66,25 @@ object WorldAccess {
       }
     }
 
+    /**
+     * Apply the given partial function to the block entity at the given
+     * position, and return the result. Useful for applying a single operation
+     * to a block entity.
+     * {{{
+     *  world.collectBlockEntity(pos) {
+     *    case b: MyBlockEntity => b.myMethod(state, world, pos)
+     *  }
+     * }}}
+     */
     def collectBlockEntity[A](pos: BlockPos)(pf: PartialFunction[BlockEntity, A]): Option[A] =
       Option(self.getBlockEntity(pos)) collect pf
   }
 
   implicit class RichBlockPos(val self: BlockPos) extends AnyVal {
+
+    /**
+     * Return the six neighbor positions of this position.
+     */
     def neighbors: Seq[BlockPos] = Direction.values.map(self.offset)
   }
 }
